84f17914e83b11dd7fb9281e35049f459aa4d3b9
georgeeset <georgeperfect4u@gmail.com>
2023-09-10 09:40:48 +0100

    more attempts to fix css
    attempt to animate lhs of landing page

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -4,14 +4,16 @@ const UPDATE_TIME_INTERVAL = 3000
 
 //  Enter URLs of your own custom images
 const imagesArray = [
-  "/static/styles/imgs/whatsapp-logo.png",
-  "/static/styles/imgs/telegram-logo.png",
+  "/static/styles/imgs/slide1.png",
+  "/static/styles/imgs/slide2.png",
+  "/static/styles/imgs/slide3.png",
+  "/static/styles/imgs/slide4.png",
 ]
 
 let i = 0
 
 setInterval(()=>{
-  if(i == 1){
+  if(i == 3){
     i = 0
   }
   else {i = i + 1};
