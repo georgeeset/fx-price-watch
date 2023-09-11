a9e2c482d04263e0c113dc015481ba1058bc1cbf
georgeeset <georgeperfect4u@gmail.com>
2023-09-11 04:44:51 +0100

    secure data added

--- fx_market_watch/settings.py
+++ fx_market_watch/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zc9t(_88!@p41-ft6%$f+zimrl%y65m@90q3m8kjht$q7k*2c8'
+SECRET_KEY = os.environ.get('DJANGO_SECRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
